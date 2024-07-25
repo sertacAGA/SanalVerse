8c54a9c114550cb9921d9f4879869ca4240efa7c
Sertaç Aydoğdu <sertacayd@gmail.com>
2024-07-25 12:45:41 +0300

    Karakter seçim ekranı ve dış mekan iç mekan güncellemeleri.
    Karakter seçim ekranı yenilendi. Dış mekan eklendi. Giriş menüleri tasarımları güncellendi.

---
+++ SanalVerse/Assets/Photon/PhotonUnityNetworking/Demos/PunBasics-Tutorial/Scripts/AvatarMenu2.cs
@@ -0,0 +1,22 @@
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Photon.Realtime;
+
+public class AvatarMenu2 : MonoBehaviour
+{
+    public GameObject avatarPrefab1;
+    public GameObject avatarPrefab2;
+    public GameObject avatarPrefab3;
+    public GameObject avatarPrefab4;
+
+    public void SelectAvatar1()
+    {
+        // Sahneyi deðiþtir
+        // SceneManager.LoadScene("PunBasics-Room for 1");
+        // PhotonNetwork.LoadLevel("Oyun");
+        // Instantiate the avatar prefab
+        PhotonNetwork.Instantiate(avatarPrefab1.name, Vector3.zero, Quaternion.identity);
+    }
+}
